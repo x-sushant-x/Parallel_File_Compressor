bf9faf317cbd2514b9fa34467a1d8e041a1bec53
Sushant <sushant.dhiman9812@gmail.com>
2025-06-21 11:04:06 +0530

    Wrote a simple method to compress file

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"compress/gzip"
 	"context"
+	"io"
+	"os"
 	"runtime"
 	"sync"
 	"time"
@@ -10,8 +13,9 @@ import (
 type CompressionConfig struct {
 	NumWorkers       int
 	Algorithm        string
-	MaxMemoryUsage   int // Bytes
+	MaxMemoryUsage   int // MegaBytes
 	CompressionLevel int
+	ChunkSize        int // MegaBytes
 }
 
 // Single File Compression Task
@@ -56,8 +60,48 @@ func NewParallelCompressor(config *CompressionConfig) *ParallelCompressor {
 		resultQueue: make(chan *CompressionResult),
 		ctx:         ctx,
 		cancel:      cancel,
-		bufferPool:  sync.Pool{},
+		bufferPool: sync.Pool{
+			New: func() interface{} {
+				return make([]byte, config.ChunkSize*1024*1024)
+			},
+		},
 	}
+}
+
+func (pc *ParallelCompressor) processJob(job *Job) *CompressionResult {
+	result := &CompressionResult{
+		Job: job,
+	}
+
+	inputFile, err := os.Open(job.InputPath)
+	if err != nil {
+		result.Error = err
+		return result
+	}
+	defer inputFile.Close()
+
+	stats, _ := inputFile.Stat()
+	size := stats.Size()
+
+	result.OriginalSize = size
+
+	outputFile, err := os.Create(job.OutputPath)
+	if err != nil {
+		result.Error = err
+		return result
+	}
+	defer outputFile.Close()
+
+	gzipWriter := gzip.NewWriter(outputFile)
+	defer gzipWriter.Close()
+
+	_, err = io.Copy(gzipWriter, inputFile)
+	if err != nil {
+		result.Error = err
+		return result
+	}
+
+	return result
 }
 
 func main() {}
